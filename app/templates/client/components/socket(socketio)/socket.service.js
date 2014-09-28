aed6fb7730aa3c60aee3dae10070dcddcecae7ec
Andrew Stuart <andrew.stuart2@gmail.com>
2014-09-27 22:42:28 -0700

    (fix) Examples are up and running.
    Don't use batarang on ngdocs sites. It breaks things.

--- app/templates/client/components/socket(socketio)/socket.service.js
+++ app/templates/client/components/socket(socketio)/socket.service.js
@@ -1,6 +1,14 @@
 /* global io */
 'use strict';
 
+/**
+ * @ngdoc service
+ * @name <%= scriptAppName %>.service:socket
+ * @description socket is a service that wraps a socket.io connection, applying angular $scope
+ * digests when messages are received. It does this so that 3-way data binding can do its magic and
+ * your data shows up in the right place.
+ */
+
 angular.module('<%= scriptAppName %>')
   .factory('socket', function(socketFactory) {
 
@@ -16,17 +24,50 @@ angular.module('<%= scriptAppName %>')
     });
 
     return {
+      /**
+       * @ngdoc
+       * @propertyOf <%= scriptAppName %>.service:socket
+       * @name <%= scriptAppName %>.service:socket#socket
+       * @type {socket.io.Socket}
+       * @description The original socket.io socket
+       */
       socket: socket,
 
       /**
-       * Register listeners to sync an array with updates on a model
+       * @ngdoc
+       * @methodOf <%= scriptAppName %>.service:socket
+       * @name <%= scriptAppName %>.service:socket#syncUpdates
+       * @description Register listeners to sync an array with updates on a model
        *
        * Takes the array we want to sync, the model name that socket updates are sent from,
        * and an optional callback function after new items are updated.
        *
-       * @param {String} modelName
-       * @param {Array} array
-       * @param {Function} cb
+       * @param {String} modelName The name of the model as expected to be sent from the socket.io server.
+       * @param {Array} array The javascript array that should be synced via socket.io
+       * @param {Function} cb A callback to be called when the `item:save` event is received.
+       * @example
+        <example module="testModule">
+          <file name="example-ctrl.js">
+            function foo($scope, socket) {
+              $scope.awesomeThings = ['foo', 'bar'];
+            }
+
+            angular.module('testModule', []).controller('foo', function($scope, socket) {
+              $scope.awesomeThings = ['angular', 'mongo', 'express', 'node'];
+
+              socket.syncUpdates('awesomeThings', $scope.awesomeThings);
+
+              $scope.$on('$destroy', function() {
+                socket.unsyncUpdates('awesomeThings');
+              });
+            });
+          </file>
+          <file name="example.html">
+            <div>
+              <div ng-bind="thing" ng-repeat="thing in awesomeThings">Awesome Things will show up here.</div>
+            </div>
+          </file>
+        </example>
        */
       syncUpdates: function (modelName, array, cb) {
         cb = cb || angular.noop;
@@ -62,10 +103,13 @@ angular.module('<%= scriptAppName %>')
       },
 
       /**
-       * Removes listeners for a models updates on the socket
-       *
-       * @param modelName
+       * @ngdoc
+       * @methodOf <%= scriptAppName %>.service:socket
+       * @name <%= scriptAppName %>.service:socket#unsyncUpdates
+       * @param {String} modelName The model name for which to no longer sync updates.
+       * @description Removes listeners for a given model's socket.io update events, triggered by Mongoose
        */
+
       unsyncUpdates: function (modelName) {
         socket.removeAllListeners(modelName + ':save');
         socket.removeAllListeners(modelName + ':remove');
