a5de41de3b33fc87fd59f5171a9b0e8921b6f161
Andrew Koroluk <koroluka@gmail.com>
2015-09-15 16:11:10 -0400

    feat(client:main): use a class for the main controller

--- app/templates/client/app/main/main.controller.js
+++ app/templates/client/app/main/main.controller.js
@@ -1,30 +1,34 @@
 'use strict';
 (function() {
 
-function MainController($scope, $http<% if (filters.socketio) { %>, socket<% } %>) {
-  var self = this;
-  this.awesomeThings = [];
+class MainController {
+  awesomeThings = [];
 
-  $http.get('/api/things').then(function(response) {
-    self.awesomeThings = response.data;<% if (filters.socketio) { %>
-    socket.syncUpdates('thing', self.awesomeThings);<% } %>
-  });
-<% if (filters.models) { %>
-  this.addThing = function() {
-    if (self.newThing === '') {
+  constructor($scope, $http<% if (filters.socketio) { %>, socket<% } %>) {
+    this.$http = $http;
+
+    $http.get('/api/things').then(response => {
+      this.awesomeThings = response.data;<% if (filters.socketio) { %>
+      socket.syncUpdates('thing', this.awesomeThings);<% } %>
+    });
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('thing');
+    });<% } %>
+  }
+
+  <% if (filters.models) { %>
+  addThing() {
+    if (this.newThing === '') {
       return;
     }
-    $http.post('/api/things', { name: self.newThing });
-    self.newThing = '';
+    this.$http.post('/api/things', { name: this.newThing });
+    this.newThing = '';
   };
 
-  this.deleteThing = function(thing) {
-    $http.delete('/api/things/' + thing._id);
+  deleteThing(thing) {
+    this.$http.delete('/api/things/' + thing._id);
   };<% } %><% if (filters.socketio) { %>
-
-  $scope.$on('$destroy', function() {
-    socket.unsyncUpdates('thing');
-  });<% } %>
 }
 
 angular.module('<%= scriptAppName %>')
